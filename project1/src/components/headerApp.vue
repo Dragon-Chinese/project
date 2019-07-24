0ad642447235f0c5af60585b7e170b9003fd5c8e
hujingyu@bloview.com <hujingyu@bloview.com>
2019-07-24 22:39:29 +0800

    新版

--- project1/src/components/headerApp.vue
+++ project1/src/components/headerApp.vue
@@ -147,7 +147,7 @@ export default {
     }
     .lang {
         p {
-            font-size:8px;
+            font-size:15px;
             font-family:FZLTHK--GBK1-0;
             font-weight:400;
             color:rgba(34,34,34,1);
